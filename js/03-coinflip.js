e179c16a4ec1b9fb81ccba09a38a1dbf7085731f
kehindeag <kenagboola@gmail.com>
2022-07-12 22:09:59 -0700

    Final

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -2,21 +2,24 @@ console.log("Hello World");
 
 //The “Coin Flip” Game
 //Usage: Use nested conditional statements
-let coinFlip = 10;
+let coinFlip;
 
 let randomNum = Math.round(Math.random() * 10);
 
+coinFlip = randomNum;
+
+let certainNum = 5;
 //alert(coinFlip);
 
 let choice = prompt("select Heads or Tails");
 
-if (coinFlip < randomNum) {
+if (coinFlip < certainNum) {
   if (choice == "Heads") {
     alert(`The flip was heads and you chose heads...you win!  ${randomNum}`);
   } else if (choice == "Tails") {
     alert(`The flip was heads but you chose tails...you lose! ${randomNum}`);
   }
-} else if (coinFlip > randomNum) {
+} else if (coinFlip > certainNum) {
   if (choice == "Heads") {
     alert(`The flip was tails but you chose heads...you lose! ${randomNum}`);
   } else if (choice == "Tails") {
